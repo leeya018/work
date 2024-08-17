42c476bf2b065fd1b18e11fc4910d94c5e3e9e56
leeya018 <leeyahav018@gmail.com>
2024-08-17 17:19:56 +0300

    s

--- src/app/shifts/page.tsx
+++ src/app/shifts/page.tsx
@@ -11,3 +11,10 @@ export default function ShiftsPage() {
     </div>
   );
 }
+
+// thisn i need
+//  db - add shift , get shifts , updateShift ( with the end time. )
+//  functions - getShiftByWorkName, getTotalHoursByName , getTotalSalarayByName,
+// items to use - hook for getign the data .
+//  components - Shifts , Shift , TotalSumShift( show hours and totalMoney)
+// interfaces - Shift - startAt , finishAt ,
