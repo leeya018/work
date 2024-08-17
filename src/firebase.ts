665383c35cde22926ea663b60963c56d2321a5b2
leeya018 <leeyahav018@gmail.com>
2024-08-17 21:51:46 +0300

    s

--- src/firebase.ts
+++ src/firebase.ts
@@ -10,16 +10,16 @@ const firebaseConfigDev = {
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+  // measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 const firebaseConfigProd = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY_PROD,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN_PROD,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID_PROD,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET_PROD,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID_PROD,
   appId: process.env.NEXT_PUBLIC_APP_ID_PROD,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID_PROD,
+  // measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID_PROD,
 };
 
 const firebaseConfig =
